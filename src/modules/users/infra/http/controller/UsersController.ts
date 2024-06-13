16adef98737e3e68ec288113ee40d755bb4b0085
Tássyla Lissa Lima <tassyla.tll@gmail.com>
2024-06-13 10:27:05 -0300

    :sparkes: create cusmomized user

--- src/modules/users/infra/http/controller/UsersController.ts
+++ src/modules/users/infra/http/controller/UsersController.ts
@@ -7,20 +7,30 @@ export default class UserController {
   public async create(req: Request, res: Response): Promise<Response> {
     const {
       name,
+      nickname,
       email,
       cpf,
+      profession,
+      specialization,
       phone,
       password,
+      city,
+      state,
     } = req.body;
 
     const createUser = container.resolve(CreateUserService);
 
     const user = await createUser.execute({
       name,
+      nickname,
       email,
       cpf,
+      profession,
+      specialization,
       phone,
       password,
+      city,
+      state,
     });
 
     user.password = '###';
