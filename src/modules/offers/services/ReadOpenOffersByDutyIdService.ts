e6f18540513ce11ea5946cfe55bc8c96bfc6aac8
Tássyla Lissa Lima <tassyla.tll@gmail.com>
2025-03-29 02:04:24 -0300

    feat: rebase

---
+++ src/modules/offers/services/ReadOpenOffersByDutyIdService.ts
@@ -0,0 +1,35 @@
+import { inject, injectable } from 'tsyringe';
+
+import { Offers } from '@prisma/client';
+
+import AppError from '@shared/errors/AppError';
+
+import IOffersRepository from '../repositories/IOffersRepository';
+import IDutiesRepository from '@modules/duties/repositories/IDutiesRepository';
+
+interface IRequest {
+  id: string;
+  duty_id: string;
+}
+
+@injectable()
+export default class ReadOpenOffersByDutyIdService {
+  constructor(
+    @inject('OffersRepository')
+    private offersRepository: IOffersRepository,
+    @inject('DutiesRepository')
+    private dutiesRepository: IDutiesRepository,
+  ) { }
+
+  public async execute({ id, duty_id }: IRequest): Promise<Offers[] | null> {
+
+    // verificar se deve ter restrições para visualizar
+    
+    const dutyExists = await this.dutiesRepository.findById(duty_id);
+    if (!dutyExists) throw new AppError('Duty with this id does not exist');
+
+    const offerEl = await this.offersRepository.findAllOpen(duty_id);
+
+    return offerEl;
+  }
+}
