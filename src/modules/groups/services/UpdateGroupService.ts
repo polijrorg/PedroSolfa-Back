f5b7f3aaec015e143b3c0fb8933a67d6d941b55b
alfredilsonito00 <augustopmello@gmail.com>
2025-03-30 14:58:26 -0300

    teste_commit

--- src/modules/groups/services/UpdateGroupService.ts
+++ src/modules/groups/services/UpdateGroupService.ts
@@ -11,7 +11,7 @@ const bucketName = process.env.BUCKET_NAME;
 const bucketRegion = process.env.BUCKET_REGION;
 const accessKey = process.env.ACCESS_KEY;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-
+// teste
 interface IRequest {
   id: string;
   name?: string;
